6d8c35c03ad972cec727af0454835bcdd82a2f3b
bekbossyn <bekbossyn.kassymkhan@gmail.com>
2019-05-25 01:09:38 +0900

    added to collection models

--- collection/admin.py
+++ collection/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Collection, Word
+
+
+@admin.register(Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'user',)
+
+
+@admin.register(Word)
+class WordAdmin(admin.ModelAdmin):
+    list_display = ('id', 'data', 'collection', 'user', 'users')
--- collection/models.py
+++ collection/models.py
@@ -5,28 +5,40 @@ from django.conf import settings
 
 # Create your models here.
 class Collection(models.Model):
-    collection = ArrayField(JSONField(default=dict, blank=True), default=list)
+    # collection = ArrayField(JSONField(default=dict, blank=True), default=list)
+    name = models.CharField(max_length=100, blank=True, null=True)
+    # data = JSONField(default=dict, blank=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='collections', null=False, on_delete=models.CASCADE)
 
     def __str__(self):
-        return u"Collection {0}".format(self.pk)
+        return u"Collection id {0} & name {1} of user {2}".format(self.pk, self.name, self.user.pk)
 
     def json(self, user=None):
+        # collections = []
+        # for index, item in enumerate(self.collection):
+        #     collections.append(item)
+        # return self.collection
+        # return collections
         return {
-            "user": self.user.json(short=False, user=self.user),
-            "phone": self.hidden_phone(user),
+            "collection_id": self.pk,
             "name": self.name,
-            "email": self.hidden_email(user),
-            "avatar": get_url(self.avatar),
-            "avatar_big": get_url(self.avatar_big) or get_url(self.avatar),
-            # "languages": [l.name for l in self.languages.all()],
-            "language_id": self.language,
-            "language": self.get_language_display(),
-            "review": self.review if self.review else None,
-            # "reviews": [r.json(user) for r in self.reviews.all()],
-            "verified": self.verified(),
-            # 'game_setting': self.game_setting.json(user),
-            "collections": self.collections.json(),
+            "words": [x.json(collection=self) for x in self.words.all()],
+            "user_id": user.pk if user else None,
+            # "collection": [x for x in self.collection],
+            # "user": self.user.json(short=True),
+            # "phone": self.hidden_phone(user),
+            # "name": self.name,
+            # "email": self.hidden_email(user),
+            # "avatar": get_url(self.avatar),
+            # "avatar_big": get_url(self.avatar_big) or get_url(self.avatar),
+            # # "languages": [l.name for l in self.languages.all()],
+            # "language_id": self.language,
+            # "language": self.get_language_display(),
+            # "review": self.review if self.review else None,
+            # # "reviews": [r.json(user) for r in self.reviews.all()],
+            # "verified": self.verified(),
+            # # 'game_setting': self.game_setting.json(user),
+            # "collections": self.collections.json(),
         }
 
     # class Meta:
@@ -35,3 +47,20 @@ class Collection(models.Model):
     #     ]
 
 
+class Word(models.Model):
+    # collection = ArrayField(JSONField(default=dict, blank=True), default=list)
+    data = JSONField(default=dict, blank=True)
+    collection = models.ForeignKey(Collection, related_name='words', blank=True, null=True, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='words', blank=True, null=True, on_delete=models.CASCADE)
+    users = ArrayField(models.IntegerField(null=True, blank=True), default=list, blank=True, null=True)
+
+    def __str__(self):
+        return u"Word {0}".format(self.pk)
+
+    def json(self, collection=None):
+        return {
+            "data": self.data,
+            "collection": self.collection.name if collection else None,
+            "user": self.user.pk if self.user else None,
+            "users": [x for x in self.users],
+        }
